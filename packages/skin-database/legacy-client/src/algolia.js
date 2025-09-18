d6ce2bc20880e0ef1dc53c6cb648de130ac89ed4
Suresh Kaleyannan <hello@suresh.app>
2025-09-18 18:51:04 +0800

    sureshks

---
+++ packages/skin-database/legacy-client/src/algolia.js
@@ -0,0 +1,43 @@
+import * as Utils from "./utils";
+import { algoliasearch } from "algoliasearch";
+
+const client = algoliasearch("HQ9I5Z6IM5", "6466695ec3f624a5fccf46ec49680e51");
+
+// Fallback search that uses SQLite. Useful for when we've exceeded the Algolia
+// search quota.
+export async function graphqlSearch(query) {
+  const queryText = Utils.gql`
+   query SearchQuery($query: String!) {
+  search_classic_skins(query: $query, first: 500) {
+    filename
+    md5
+    nsfw
+  }
+}`;
+  const data = await Utils.fetchGraphql(queryText, { query });
+  const hits = data.search_classic_skins.map((skin) => {
+    return {
+      objectID: skin.md5,
+      fileName: skin.filename,
+      nsfw: skin.nsfw,
+    };
+  });
+  return { hits };
+}
+
+export async function algoliaSearch(query, options = {}) {
+  const result = await client.searchSingleIndex({
+    indexName: "Skins",
+    searchParams: {
+      query,
+      attributesToRetrieve: ["objectID", "fileName", "color", "nsfw"],
+      attributesToHighlight: [],
+      hitsPerPage: 1000,
+      // https://www.algolia.com/doc/api-reference/api-parameters/typoTolerance/
+      // min: Retrieve records with the smallest number of typos.
+      typoTolerance: "min",
+      ...options,
+    },
+  });
+  return result;
+}
